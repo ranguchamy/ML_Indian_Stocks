44e58e2cd90bd8e91165e2245ab28f6e30677418
Raja Angusamy <ranguchamy@gmail.com>
2024-01-07 21:45:40 +0530

    UI Path Initial Check In

---
+++ UiPath/Browser_Tradinview_Automation/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace Browser_Tradinview_Automation
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
